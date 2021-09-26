8c35ed3ff41c5d433d000cd4edf7ec07ace79724
ATGSilva <56297617+ATGSilva@users.noreply.github.com>
2021-09-26 14:57:41 +0100

    Foundations for rebuild
    Removed all files other than those such as documentation, licence, gitignore, readme and data.

--- claude_setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(
-	include_dirs=[numpy.get_include()],
-    ext_modules = cythonize("*.pyx", compiler_directives={'language_level' : "3"})
-)
